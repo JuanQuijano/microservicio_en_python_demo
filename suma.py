963b0f1992a61cd2caee7ac12911f823f8ee3c66
Juan Quijano <listas@juanquijano.es>
2024-06-02 22:04:20 +0000

    Funciona el microservicio de suma, todavía no la ejecución del test

---
+++ suma.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+def sumar(a, b):
+    try:
+        resultado = a + b
+        return resultado
+    except Exception as e:
+        return f"Error al sumar: {str(e)}"
+
+@app.route("/sumar", methods=["POST"])
+def sumar_handler():
+    try:
+        data = request.get_json()
+        cantidad1 = float(data["cantidad1"])
+        cantidad2 = float(data["cantidad2"])
+        resultado_suma = sumar(cantidad1, cantidad2)
+        return jsonify({"resultado": resultado_suma})
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+if __name__ == "__main__":
+    app.run(debug=True)
